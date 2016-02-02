106973cf3a9e613bee8fdf6e9ab83f4262655619
PKuhn <patrick.kuhyn@student.hpi.de>
2016-02-02 22:40:50 +0100

    Download articel present in feeds

--- models/Grabber.py
+++ models/Grabber.py
@@ -1,4 +1,5 @@
 import feedparser
+import requests
 
 """
 This class represents a grabber. It is responsible for
@@ -9,6 +10,8 @@ Additionally it implements the entire logic of a specific
 grab. That means downloading the source code of the articles
 in an rss feed
 """
+
+
 class Grabber:
 
     def __init__(self, name, feed, db=('localhost', 27017)):
@@ -24,4 +27,15 @@ class Grabber:
         # here. Use the requests lib to download the articles
 
         data = feedparser.parse(self.feed)
+        for rss_item in data['entries']:
+            self.store_rss_item(rss_item)
         print([entry['title_detail']['value'] for entry in data['entries']])
+
+    def download_article(self, article_url):
+        r = requests.get(article_url)
+        print(r.text)
+
+    def store_rss_item(self, rss_item):
+        article_url = rss_item['link']
+        article = self.download_article(article_url)
+
