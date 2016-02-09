7d4b8b5a050270d13d6ef33c6df5ac105ac166f5
bitbender <loster.michael@gmail.com>
2016-02-09 17:19:53 +0100

    Add comments and change config file

--- config.py
+++ config.py
@@ -6,16 +6,31 @@ class Config(object):
     This class implements a config holder. The holder is responsible for
     loading the current configuration from a YAML file and by doing so
     making the configuration accessable.
+
+    The config holder implements the singleton design pattern.
     """
     _instance = None
 
-    def __init__(self, file='config.yml'):
+    def __init__(self, file):
+        """
+        The constructor of this class.
+
+        It creates a Config object that contains all the config
+        parameters defined in the config file or, if no config
+        file is specified, a Config object that is empty
+
+        :param file: a file from which to load the configuration
+        """
 
         if Config._instance is not None:
             raise NotImplemented("This is a singleton class. Use the get_instance() method")
 
-        with open(file, 'r') as ymlfile:
-            self.config = yaml.load(ymlfile)
+        if file:
+            self.file = file
+            with open(file, 'r') as ymlfile:
+                self.config = yaml.load(ymlfile)
+        else:
+            self.config = {}
 
     @staticmethod
     def get_instance():
@@ -29,8 +44,22 @@ class Config(object):
         return Config._instance
 
     def load(self, file):
+        """
+        This method loads a config file into the Config object
+
+        :param file: a file that should be loaded into the config object
+        """
+        self.file = file
         with open(file, 'r') as ymlfile:
             self.config = yaml.load(ymlfile)
+
+    def save(self):
+        """
+        This method saves the config object to a config file.
+        :return:
+        """
+        with open(self.file, 'w') as ymlfile:
+            ymlfile.write(yaml.dump(self.config))
 
     def __getitem__(self, item):
         return self.config[item]
