38e9fa6276165903a607b0a0168f1c053e10265f
bitbender <loster.michael@gmail.com>
2016-02-21 23:19:31 +0100

    Add interval units to create menu. Load grabbers from webservice in Manage Grabbers view.

--- client/app/scripts/controllers/grabber.js
+++ client/app/scripts/controllers/grabber.js
@@ -9,6 +9,43 @@
 angular.module('yapp')
   .controller('GrabberCtrl', function ($scope, $state, ngDialog, $http) {
 
+    /**
+    * Convert the interval value to the correct unit (seconds,minutes,hours)
+    * @param value the inverval value in seconds
+    * @param unit the unit it should be converted to (sec, min, h)
+    * @returns number converted interval value
+    */
+    var convert = function(value,unit){
+      if(unit == 'min'){
+        return value * 60;
+      }else if(unit == 'h'){
+        return value * 60 * 60;
+      }else{
+        return value;
+      }
+    };
+
+    $scope.getAll = function () {
+
+      var req = {
+        method: 'GET',
+        url: 'http://localhost:5000/grabber',
+        headers: {
+          'Content-Type': "application/json"
+        }
+      };
+
+      $http(req).then(function successCallback(response) {
+        $scope.grabbers = response.data;
+        console.log(response.data)
+      }, function errorCallback(response) {
+        console.log('Ups for some reason I couldn\'t create the grabber')
+      });
+
+    };
+    // get all data when page reloads
+    $scope.getAll();
+
     $scope.clickToOpen = function () {
       ngDialog.open({
         template: '../../views/partials/create_grabber.html',
@@ -16,11 +53,9 @@ angular.module('yapp')
       });
     };
 
-    $scope.greet = function () {
-      console.log($scope.grabber);
-    };
-
     $scope.create = function () {
+      $scope.grabber.interval = convert($scope.grabber.interval, $scope.unit);
+
       var req = {
         method: 'POST',
         url: 'http://localhost:5000/grabber',
@@ -36,5 +71,5 @@ angular.module('yapp')
         console.log('Ups for some reason I couldn\'t create the grabber')
       });
 
-    }
+    };
   });
