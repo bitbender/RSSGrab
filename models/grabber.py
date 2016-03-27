ca7e449aa9fcac36a098c48d205502e44e201f15
bitbender <loster.michael@gmail.com>
2016-03-27 22:34:49 +0200

    Refactor the grabber and adjust the affected server code

--- models/grabber.py
+++ models/grabber.py
@@ -1,10 +1,13 @@
 from bson import ObjectId
 import feedparser
+import logging
 import requests
 import time
+import copy
 
 from config import Config
 from smpl_conn_pool import SmplConnPool
+
 """
 This class represents a grabber. It is responsible for
 storing all the information needed in order to grab the
@@ -19,12 +22,16 @@ in an rss feed
 class Grabber:
     cfg = Config.get_instance()
 
-    def __init__(self, name, feed, interval, _id=None):
+    def __init__(self, name, feed, interval, _id=ObjectId()):
+
+        if type(_id) == str:
+            self._id = ObjectId(_id)
+        else:
+            self._id = _id
+
         self.name = name
         self.feed = feed
         self.interval = interval
-        if _id:
-            self._id = ObjectId(_id)
 
     def run(self):
         data = feedparser.parse(self.feed)
@@ -39,21 +46,28 @@ class Grabber:
         response.raise_for_status()
 
     def save(self):
+        """
+        Save the grabber to a mongodb database.
+
+        :return: the document id of the grabber in the database.
+        """
         connection = SmplConnPool.get_instance().get_connection()
         grabber_collection = connection[
             Grabber.cfg['database']['db']]['grabbers']
         grabber_id = grabber_collection.insert_one(self.__dict__).inserted_id
-        print('Saved grabber with id {}'.format(grabber_id))
+        logging.info('Saved grabber with id {}'.format(grabber_id))
         self._id = ObjectId(grabber_id)
         return grabber_id
 
     def store_rss_item(self, rss_item):
         article_url = rss_item['link']
         rss_item['article'] = self.download_article(article_url)
+
         connection = SmplConnPool.get_instance().get_connection()
         feed_collection = connection[Grabber.cfg['database']['db']][
             Grabber.cfg['database']['collections']['articles']]
         db_feed = feed_collection.find({'id': rss_item['id']})
+
         assert db_feed.count() < 2, "id should be a primary key"
         if not db_feed.count() > 0:
             feed_collection.save(rss_item)
@@ -61,7 +75,12 @@ class Grabber:
             self.update_feed(db_feed[0], rss_item)
 
     def update_feed(self, old_feed, new_feed):
-        # Replaces old feed with new one if it is published at another time
+        """
+        Replaces old feed with new one if it is published at another time
+        :param old_feed:
+        :param new_feed:
+        :return:
+        """
         assert old_feed['id'] == new_feed['id'], 'Updates should only \
                 be made to a newer version of the article'
         if self.is_newer(old_feed['published'], new_feed['published']):
@@ -76,10 +95,37 @@ class Grabber:
         new = time.strptime(new_date.replace(',', ''), '%a %d %b %Y %X %z')
         return old < new
 
-    def to_json(self):
-        return {
-            'id': str(self._id),
-            'name': self.name,
-            'feed': self.feed,
-            'interval': self.interval
-        }
+    def encode(self, rm=None):
+        """
+        Encode the grabber as a dictionary
+
+        :param rm: the fields of the grabber that should me excluded from serialization
+        :return: a dictionary encoding of the grabber
+        """
+
+        if rm is None:
+            rm = []
+        else:
+            rm = rm.split(',')
+
+        # Copy the dictionary with all attributes of this object.
+        result = copy.deepcopy(self.__dict__)
+        # Convert the ObjectId into a string
+        result['_id'] = str(self._id)
+
+        for key in rm:
+            if key in result:
+                del result[key]
+
+        return result
+
+    @staticmethod
+    def decode(doc):
+        assert type(doc) == dict
+        if '_id' in doc:
+            return Grabber(doc['name'], doc['feed'], doc['interval'], doc['_id'])
+        else:
+            return Grabber(doc['name'], doc['feed'], doc['interval'])
+
+    def __repr__(self):
+        return '[{0}, {1}, {2}, {3}]'.format(self._id, self.name, self.feed, self.interval)
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import logging
 from bson import ObjectId
 from flask import Flask, request
 from config import Config
@@ -5,10 +6,13 @@ from smpl_conn_pool import SmplConnPool
 from models.grabber import Grabber
 from flask.ext.cors import CORS
 from apscheduler.schedulers.background import BackgroundScheduler
-import json
+from json import dumps
 
 # load configuration
 conf = Config.get_instance()
+
+# configure logger
+logging.basicConfig(level=logging.DEBUG)
 
 # init connection pool
 conn = SmplConnPool.get_instance()
@@ -19,10 +23,6 @@ scheduler = BackgroundScheduler()
 app = Flask(__name__)
 # Add standard CORS headers in order to access the app
 CORS(app)
-
-# stores all grabbers currently in the system
-# TODO: This needs to be a dictionary for quick removal of grabbers
-grabbers = []
 
 # a mapping from a specific grabber to its corresponding job
 grabber_to_job = {}
@@ -35,38 +35,52 @@ def index():
 
 @app.route('/grabber', methods=['GET'])
 def get_all_grabbers():
-    return json.dumps([grabber.to_json() for grabber in grabbers])
+    connection = SmplConnPool.get_instance().get_connection()
+    grabber_collection = connection[
+        Grabber.cfg['database']['db']]['grabbers']
+
+    return dumps([grabber.encode() for grabber in [Grabber.decode(doc) for doc in grabber_collection.find()]])
 
 
 @app.route('/grabber', methods=['POST'])
 def add_grabber():
-    # TODO: Validate the incoming data
     jsn = request.get_json()
-    database_id = _add_grabber(jsn['name'], jsn['feed'], jsn['interval'])
-    return '{}'.format(database_id), 201
+    # TODO: Validate the incoming json data
+    grabber = _add_grabber(jsn)
+    return '{}'.format(grabber.encode()), 201
 
 
-@app.route('/grabber/<_id>', methods=['PUT'])
-def update_grabber(_id):
+@app.route('/grabber', methods=['PUT'])
+def update_grabber():
     jsn = request.get_json()
     connection = SmplConnPool.get_instance().get_connection()
     grabber_collection = connection[
         Grabber.cfg['database']['db']]['grabbers']
-    grabber = Grabber(jsn['name'], jsn['feed'], jsn['interval'], _id)
+
+    grabber = Grabber.decode(jsn)
     _update_job(grabber)
+
+    update = grabber.encode()
+    del update['_id']
+
     grabber_collection.find_one_and_replace(
-        {"_id": ObjectId(_id)},
-        jsn
+        {"_id": grabber._id},
+        update
     )
     return 'Succesfully updated grabber', 201
 
 
-def _add_grabber(name, feed, interval):
-    new_grabber = Grabber(name, feed, interval)
-    database_id = new_grabber.save()
-    _add_job_for_grabber(new_grabber)
-    grabbers.append(new_grabber)
-    return database_id
+def _add_grabber(doc):
+    """
+    Create and save grabber from a json document / dict
+
+    :param doc: the document which should be used to create the grabber
+    :return: the newly created grabber
+    """
+    grabber = Grabber.decode(doc)
+    grabber.save()
+    _add_job_for_grabber(grabber)
+    return grabber
 
 
 def _add_job_for_grabber(grabber):
@@ -75,10 +89,9 @@ def _add_job_for_grabber(grabber):
 
 
 def _update_job(grabber):
-    object_id = ObjectId(grabber._id)
-    assert object_id in grabber_to_job
-    if object_id in grabber_to_job:
-        job = grabber_to_job[object_id]
+    assert grabber._id in grabber_to_job
+    if grabber._id in grabber_to_job:
+        job = grabber_to_job[grabber._id]
         job.remove()
     _add_job_for_grabber(grabber)
 
@@ -110,53 +123,44 @@ def stop_grabber(_id):
         return 'Invalid grabber id', 404
 
 
-@app.route('/grabber/<id>', methods=['DELETE'])
-def delete_grabber(id):
+@app.route('/grabber', methods=['DELETE'])
+def delete_grabber():
     """
     Delete grabber from system
     :param id: the id of the grabber that should be deleted
     :return: http code 200 if grabber could be deleted, else 404
     """
-    object_id = ObjectId(id)
-    if object_id in grabber_to_job:
-
+    grabber = Grabber.decode(request.get_json())
+    if grabber._id in grabber_to_job:
         # delete associated job
-        job = grabber_to_job[object_id]
+        job = grabber_to_job[grabber._id]
         job.remove()
-        del grabber_to_job[object_id]
-
-        # FIXME: stupid an inefficient but using the grabbers collection the only way
-        # delete grabber from grabbers collection
-        for idx, grabber in enumerate(grabbers):
-            if str(grabber._id) == id:
-                del grabbers[idx]
+        del grabber_to_job[grabber._id]
 
         # delete grabber from database
         connection = SmplConnPool.get_instance().get_connection()
         grabber_collection = connection[Grabber.cfg['database']['db']]['grabbers']
-        grabber_collection.remove({'_id': ObjectId(id)})
+        grabber_collection.remove({'_id': grabber._id})
 
-        return 'Succesfully stopped grabber', 200
+        return 'Grabber deleted', 200
     else:
         return 'Invalid grabber id', 404
 
 
 def main():
     scheduler.start()
-    fetch_grabbers_from_db()
+    restart_grabbers_from_db()
     app.run()
 
 
-def fetch_grabbers_from_db():
+def restart_grabbers_from_db():
     connection = SmplConnPool.get_instance().get_connection()
     grabber_collection = connection[Grabber.cfg['database']['db']]['grabbers']
-    for grabber in grabber_collection.find():
-        recreated = Grabber(grabber['name'], grabber['feed'], grabber['interval'],
-                            grabber['_id'])
-        grabbers.append(recreated)
-        job = scheduler.add_job(recreated.run, 'interval',
-                                seconds=grabber['interval'])
-        grabber_to_job[grabber['_id']] = job
+    for doc in grabber_collection.find():
+        grabber = Grabber.decode(doc)
+        job = scheduler.add_job(grabber.run, 'interval',
+                                seconds=grabber.interval)
+        grabber_to_job[grabber._id] = job
 
 
 if __name__ == '__main__':
